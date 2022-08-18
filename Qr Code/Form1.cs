ce491bc36c763a3575d29d5b1be7b3e23d7a7c57
HashJProgramming <joshuamarceloambalong@gmail.com>
2022-08-18 10:32:09 +0800

    Add project files.

---
+++ Qr Code/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Qr_Code
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            QRCoder.QRCodeGenerator OG = new QRCoder.QRCodeGenerator();
+            var Mydata = OG.CreateQrCode(textBox1.Text, QRCoder.QRCodeGenerator.ECCLevel.Q);
+            var code = new QRCoder.QRCode(Mydata);
+            pictureBox1.Image = code.GetGraphic(20);
+
+        }
+    }
+}
